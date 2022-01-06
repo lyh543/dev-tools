6b140dd6d605503232f340b8be211fa5d6d84dae
lyh543 <lyh543@outlook.com>
2022-01-06 13:06:01 +0800

    fix ffmpeg720.ps1

--- ffmpeg720.ps1
+++ ffmpeg720.ps1
@@ -1,10 +1,11 @@
-if ($args[1].Equals('')) {
-	Write-Output "Usage: ffmpeg720 <input_video> <output.mp4>"
+if ($null -eq $args[1]) {
+    Write-Output "Usage: ffmpeg720 <input_video> <output.mp4>"
 } else {
-	ffmpeg -hwaccel cuda `	# cuda 加速
-		-i $args[0] `		# 输入文件
-		-b:v 2M `			# 码率
-		-vf scale=-1:720 `	# 高度为 720p，宽度按比例调整
-		-c:v hevc_nvenc `	# h. 265 编码
-		$args[1]			# 输出文件
+    ffmpeg `
+        -hwaccel cuda `
+        -i $args[0] `
+        -b:v 2M `
+        -vf scale=-1:720 `
+        -c:v hevc_nvenc `
+        $args[1]
 }
