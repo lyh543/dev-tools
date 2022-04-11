856be973cd80c07b8f8e4f1c46b2417306b73e7f
lyh543 <lyh543@outlook.com>
2022-04-11 23:12:49 +0800

    feat(netsh-port): just print infomation and command

--- netsh-port.ps1
+++ netsh-port.ps1
@@ -1,8 +1,12 @@
-switch ($args[0]) {
-    "list" { netsh interface ipv4 show excludedportrange protocol=tcp }
-    "add" { netsh interface ipv4 add excludedportrange protocol=tcp numberofports=100 startport=$args[1] }
-    "del" { netsh interface ipv4 delete excludedportrange protocol=tcp numberofports=100 startport=$args[1] }
-    Default {
-        Write-Output "Usage: $0 [list|add <startport>|del <startport>]"
-    }
-}
+#####################################################################################################################
+#                                               HOW TO USE NETSH                                                    #
+#                                                                                                                   #
+#     set dynamic portrange: netsh int ipv4 set dynamicport tcp start=50000 num=255                                 #
+#   list excluded portrange: netsh int ipv4 show excludedportrange protocol=tcp                                     #
+#    add excluded portrange: netsh int ipv4 add excludedportrange protocol=tcp numberofports=100 startport=50000    #
+# delete excluded portrange: netsh int ipv4 delete excludedportrange protocol=tcp numberofports=100 startport=50000 #
+#####################################################################################################################
+
+netsh interface ipv4 show dynamicport tcp
+netsh interface ipv4 show excludedportrange protocol=tcp
+Get-Content $PSCommandPath
