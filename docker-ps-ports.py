63c42dee3fe2e7d5a4b4c34a5f453bc6549e1364
lyh543 <lyh543@outlook.com>
2022-05-15 11:27:08 +0800

    feat(docker-ps-ports): remove ipv6 addr and shorten ipv4 addr from result

--- docker-ps-ports.py
+++ docker-ps-ports.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+import subprocess
+import re
 from __init__ import *
 
 # https://docs.docker.com/engine/reference/commandline/ps/#formatting
-system(
-    'docker ps --format "table {{.Names}}\t{{.Ports}}"',
-)
+command = 'docker ps --format "table {{.Names}}\t{{.Ports}}"'
+result = subprocess.check_output(command, shell=True, text=True)
+
+result = re.sub(r'0.0.0.0:', '0:', result)
+# remove ipv6 ports since it's too long
+result = re.sub(r':::.*?, ', '', result)
+result = re.sub(r':::.*?\n', '\n', result)
+print(result)
