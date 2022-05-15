3ab3fefcd4cb76fac01efb2ccf64047fd444f953
lyh543 <lyh543@outlook.com>
2022-05-15 11:35:29 +0800

    chore(git hooks): run dev-tools-install-git-repo before commit

--- dev-tools-install-git-repo.py
+++ dev-tools-install-git-repo.py
@@ -4,19 +4,28 @@ from __init__ import *
 
 print('install or update git hooks and other configs...', end='', flush=True)
 
+# config dir
 DEV_TOOLS_ROOT = MAIN_FILE.parent
 DEV_TOOLS_GIT_HOOKS = DEV_TOOLS_ROOT / '.git' / 'hooks'
 
+# commands to be run in the hook
 POSIX_GIT_PRE_COMMIT = f"""#!/bin/sh
 chmod a+x *.py
 git update-index --chmod=+x *.py
+echo add execute permission to *.py...done
+{DEV_TOOLS_ROOT}/dev-tools-install-git-repo.py
 """
+
 POSIX_GIT_POST_MERGE = f"""#!/bin/sh
 {DEV_TOOLS_ROOT}/dev-tools-install-git-repo.py
 """
+
 WINDOWS_GIT_PRE_COMMIT = f"""#!/bin/sh
 git update-index --chmod=+x *.py
+echo add execute permission to *.py...done
+{DEV_TOOLS_ROOT}/dev-tools-install-git-repo.py
 """
+
 WINDOWS_GIT_POST_MERGE = f"""#!/bin/sh
 python {DEV_TOOLS_ROOT.as_posix()}/dev-tools-install-git-repo.py
 """
@@ -31,7 +40,7 @@ def update_hook(name: str, content: str):
 update_hook('pre-commit', WINDOWS_GIT_PRE_COMMIT if isWindows else POSIX_GIT_PRE_COMMIT)
 update_hook('post-merge', WINDOWS_GIT_POST_MERGE if isWindows else POSIX_GIT_POST_MERGE)
 
-print('done')
+print('done', flush=True)
 
 if not isWindows:
     system(f'python {DEV_TOOLS_ROOT}/dev-tools-generate-shell.py')
