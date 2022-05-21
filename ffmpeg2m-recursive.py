695c30b5d90018968e5a5f6d0eb80daa7a2ae016
lyh543 <lyh543@outlook.com>
2022-05-21 10:39:48 +0800

    fix(ffmpeg2m-recursive): filter compressed and origin mp4 file

--- ffmpeg2m-recursive.py
+++ ffmpeg2m-recursive.py
@@ -12,15 +12,17 @@ def ffmpeg2m_recursive(path="."):
     file_list = traverse(path,
                          filename_filter=extension_allow_filter(VIDEO_FILE_EXTENSIONS),
                          dirname_filter=filename_block_filter(COMMON_IGNORED_DIRECTORIES))
-    filtered_file_list = list(filter(lambda f: f.split('.')[-2] != 'compressed', file_list))
+    filtered_file_list = list(filter(lambda f: f.split('.')[-2] != 'compressed' and
+                                               f.split('.')[:-1] + '.compressed.mp4' not in file_list,
+                                     file_list))
     output_list = list(map(lambda f: '.'.join(f.split('.')[:-1]) + '.compressed.mp4', filtered_file_list))
 
     input_output_list = list(zip(filtered_file_list, output_list))
     input_output_str = map(lambda x: ' -> '.join(x), input_output_list)
-    print("file_list:", "\n".join(input_output_str),  sep='\n', end='\n\n')
+    print("file_list:", "\n".join(input_output_str), sep='\n', end='\n\n')
     for i in range(len(input_output_list)):
         [input, output] = input_output_list[i]
-        print(f'[{i+1}/{len(input_output_list)}] ', end='')
+        print(f'[{i + 1}/{len(input_output_list)}] ', end='')
         value = ffmpeg2m(input, output, overwrite=False)
         if value == 255:
             exit(value)
