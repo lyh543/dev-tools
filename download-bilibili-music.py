8964e4feaf5a647ac33f0b94b477ae9cde647246
lyh543 <lyh543@outlook.com>
2023-06-10 17:13:13 +0800

    download-bilibili-music support multipart

--- download-bilibili-music.py
+++ download-bilibili-music.py
@@ -8,30 +8,35 @@ import pathlib
 TITLE_REGEXP = re.compile(r"<title.*?>(.*)_哔哩哔哩_bilibili</title>")
 
 
-def get_bilibli_url(bv: str) -> str:
-    return f"https://www.bilibili.com/video/{bv}"
+def clean_filename(name: str) -> str:
+    return re.sub(r"[\/\\\:\*\?\"\<\>\|]", "_", name)
 
 
-def get_bilibili_video_title(bv: str) -> str:
-    url = get_bilibli_url(bv)
+def parse_bv_from_url(url: str) -> str:
+    re_search_result = re.search(r"BV(\w+)", url)
+    return re_search_result.group(1)
+
+
+def get_bilibili_video_title(url: str) -> str:
     response = requests.get(url)
     re_search_result = re.search(TITLE_REGEXP, response.text)
     return re_search_result.group(1)
 
 
-def download_bilibili_video(bv: str, save_dir: pathlib.Path, name: str):
-    url = get_bilibli_url(bv)
+def download_bilibili_video(url: str, save_dir: pathlib.Path, name: str):
     os.system(f"you-get -o {save_dir} -O {name} {url}")
 
 
 if __name__ == "__main__":
-    [bv] = argparse(["bv"], rest="error")
+    [url] = argparse(["url"], rest="error")
+    bv = parse_bv_from_url(url)
     save_dir = pathlib.Path.cwd()
     video_path = save_dir / f"{bv}.mp4"
     music_path = save_dir / f"{bv}.mp3"
 
-    title = get_bilibili_video_title(bv)
-    download_bilibili_video(bv, save_dir, bv)
+    title = get_bilibili_video_title(url)
+    clean_title = clean_filename(title)
+    download_bilibili_video(url, save_dir, bv)
     ffmpeg(
         str(video_path),
         str(music_path),
@@ -39,5 +44,5 @@ if __name__ == "__main__":
         overwrite="always",
         audio_bitrate="256K",
     )
-    music_path.rename(music_path.parent / f"{title}.mp3")
-    print(f"Done! {title}.mp3")
+    music_path.rename(music_path.parent / f"{clean_title}.mp3")
+    print(f"Done! {clean_title}.mp3")
