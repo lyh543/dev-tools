94609b4fa71045c1822d7973b9be421b04f941e3
lyh543 <lyh543@outlook.com>
2024-02-20 10:45:16 +0800

    update blog path

--- p.py
+++ p.py
@@ -6,7 +6,7 @@ from datetime import datetime
 print("\npushing to lyh543/blog\n")
 
 system(
-    "pushd ~/git/blog/" + REDIRECT_STDOUT_TO_NULL,
+    "pushd ~/git/github/blog/" + REDIRECT_STDOUT_TO_NULL,
     "git add --all",
     f"git commit -m 'blog: update on {datetime.now()}'",
     "git push origin master",
